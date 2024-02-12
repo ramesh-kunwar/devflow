7343e113868b3d7a3f86599b9c5a01f85092a3a3
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2024-02-12 20:06:46 +0545

    add auth

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,8 @@
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      {children}
+    </div>
+  );
+};
+export default Layout;
--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-const Home = () => {
-  return (
-    <div className="">
-      <h1 className="h1-bold">DevFlow</h1>
-    </div>
-  );
-};
-export default Home;
