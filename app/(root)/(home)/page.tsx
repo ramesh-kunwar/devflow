fc4730a0626aac529c2cf94a151a124254109cfe
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2024-02-14 14:21:56 +0545

    navbar added

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,10 +1,4 @@
-import { UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   // console.log("object");
-  return (
-    <div className="h-screen">
-      <UserButton afterSignOutUrl="/" />
-    </div>
-  );
+  return <div className="h-screen"></div>;
 }
