a1c403d050f20fae06a063aeb3cf3099a0fe335d
Ramesh Kunwar <rameshkunwar1110@gmail.com>
2024-02-15 15:53:11 +0545

    Home page noResult UI

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,41 @@
 import HomeFilters from "@/components/home/HomeFilters";
 import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
 import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
 import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
+const questions = [
+  {
+    _id: 1,
+    title: "Cascading deletes in SQLAlchemy?",
+    tags: [
+      { _id: 1, name: "python" },
+      { _id: 2, name: "sql" },
+    ],
+    author: "John Doe",
+    upvotes: 100,
+    views: 100,
+    answers: 2,
+    createdAt: "2021-09-01T12:00:00:000Z",
+  },
+  {
+    _id: 2,
+    title: "How to center a div?",
+    tags: [
+      { _id: 1, name: "css" },
+      { _id: 2, name: "sql" },
+    ],
+    author: "John Doe",
+    upvotes: 50,
+    views: 90,
+    answers: 2,
+    createdAt: "2021-09-01T12:00:00:000Z",
+  },
+];
+
 export default function Home() {
-  // console.log("object");
   return (
     <div>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
@@ -32,6 +61,23 @@ export default function Home() {
         />
       </div>
       <HomeFilters />
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {/* looping through questions */}
+
+        {questions.length > 0 ? (
+          questions.map((question) => {
+            return <div>question card</div>;
+          })
+        ) : (
+          <NoResult
+            title="There are no questions to show."
+            description="Be the first to break the silence, 🚀 Ask a question and kickstart the discussion. Our Query could be the next big thing others learn from. Get invloved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </div>
     </div>
   );
 }
